e1abb84202562d0e3ed5e1d832171b54dd13946a
Daniel Torres <danielteodolino@gmail.com>
2019-07-02 21:51:37 -0300

    Extremamente Básico
    Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
    
    Entrada
    A entrada contém 2 valores inteiros.
    
    Saída
    Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha. Cuide para que tenha um espaço antes e depois do sinal de igualdade, conforme o exemplo abaixo.

---
+++ 1003_somaSimples.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+	
+	int a, b, SOMA;
+	
+	scanf("%d", &a);
+	scanf("%d", &b);
+	
+	SOMA = a+b;
+	
+	printf("SOMA = %d\n", SOMA);
+	
+	return 0;
+}
